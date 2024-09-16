5fa9a2d18d1e652cbbd4627781d4b7ad2f648c3b
Michael <Magawrych85@gmail.com>
2024-09-15 23:39:01 -0400

    push again

--- script.js
+++ script.js
@@ -11,15 +11,15 @@
 // I IS THE VARIABLE NAME
 
 
-// for (let i = 1; i <= 100; i++){
-//     if (i % 3 === 0 && i % 5 ===0){
-//         console.log(`Fizz Buzz`);
-//     } else if (i % 3 === 0){
-//         console.log(`Fizz`);
-//     } else if (i % 5 ===0){
-//         console.log(`Buzz`)
-//     } else {console.log(i)}
-// }   
+for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 ===0){
+        console.log(`Fizz Buzz`);
+    } else if (i % 3 === 0){
+        console.log(`Fizz`);
+    } else if (i % 5 ===0){
+        console.log(`Buzz`)
+    } else {console.log(i)}
+}   
 
 
 
